87b7eddf7b4deff8841e59cbd4ac14fb52b6c696
wouow <aa3b3c2tw@gmail.com>
2024-01-04 12:15:47 +0800

    scifi map

--- Assets/Scripts/PlayerMovementAdvanced.cs
+++ Assets/Scripts/PlayerMovementAdvanced.cs
@@ -344,7 +344,7 @@ public class PlayerMovementAdvanced : MonoBehaviour
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
 
         // turn gravity off while on slope
-        if(!wallrunning) rb.useGravity = !OnSlope();
+        //if(!wallrunning) rb.useGravity = !OnSlope();
     }
 
     private void SpeedControl()
